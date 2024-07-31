cc11821168fe1412f8449dc418b13b3d66da027a
ajswetz <ajswetz@icloud.com>
2024-07-31 09:05:20 -0400

    add LeafNode child class and tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -24,3 +24,26 @@ class HTMLNode:
             children: {self.children}
             props: {self.props}
             '''
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, value, tag=None, children=None, props=None):
+        if children != None:
+            raise ValueError("Objects of class LeafNode must not have any children.")
+        
+        if value == None:
+            raise ValueError("Objects of class LeafNode must have a 'value' defined.")
+        
+        super().__init__(tag=tag, value=value, children=None, props=props)
+
+    def to_html(self):
+        
+        if not self.tag:
+            return self.value
+        
+        if self.props:
+            html_prop_str = self.props_to_html()
+            return f"<{self.tag}{html_prop_str}>{self.value}</{self.tag}>"
+        
+        else:
+            return f"<{self.tag}>{self.value}</{self.tag}>"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -10,6 +10,25 @@ class TestHTMLNode(unittest.TestCase):
         self.assertIsInstance(props_to_html, str)
         self.assertEqual(props_to_html[0], " ")
         self.assertIn(' href="https://me.com"', props_to_html)
+
+class TestLeafNode(unittest.TestCase):
+    def test_no_children(self):
+        self.assertRaises(ValueError, LeafNode, value="test", children=["child1", "child2"])
+
+    def test_value_required(self):
+        self.assertRaises(ValueError, LeafNode, value=None)
+
+    def test_no_tag(self):
+        leaf = LeafNode(value="This should return as raw text.")
+        test_to_html = leaf.to_html()
+        self.assertEquals(leaf.value, test_to_html)
+
+    def test_props(self):
+        leaf = LeafNode(tag="a", value="Click me!", props={"href": "https://www.google.com"})
+        test_to_html = leaf.to_html()
+        props_to_html = leaf.props_to_html()
+        self.assertIn(leaf.tag, test_to_html)
+        self.assertIn(props_to_html, test_to_html)
 
 if __name__ == "__main__":
     unittest.main()
