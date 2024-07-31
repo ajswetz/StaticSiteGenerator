eee79bc365f1d2f235b4966cea02ab2b03b045e9
ajswetz <ajswetz@icloud.com>
2024-07-30 20:51:30 -0400

    add textnode.py

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from textnode import TextNode
+
+def main():
+    my_TextNode = TextNode("Testing text node", "italic", "https://me.com")
+
+    print(my_TextNode)
+
+main()
---
+++ src/textnode.py
@@ -0,0 +1,18 @@
+class TextNode:
+    def __init__(self, text, text_type, url=None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, other_TextNode) -> bool:
+        if self.text != other_TextNode.text:
+            return False
+        if self.text_type != other_TextNode.text_type:
+            return False
+        if self.url != other_TextNode.url:
+            return False
+
+        return True
+
+    def __repr__(self) -> str:
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
