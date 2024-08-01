1b4d496fae1e5b1042a83bac7daffb7edfe23282
ajswetz <ajswetz@icloud.com>
2024-07-31 22:06:45 -0400

    add ParentNode tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -30,33 +30,33 @@ class LeafNode(HTMLNode):
     def __init__(self, value, tag=None, children=None, props=None):
         if children:
             raise ValueError("Objects of class LeafNode must not have any children.")
-        
+
         if not value:
             raise ValueError("Objects of class LeafNode must have a 'value' defined.")
-        
+
         super().__init__(tag=tag, value=value, children=None, props=props)
 
     def to_html(self):
-        
+
         if not self.tag:
             return self.value
-        
+
         if self.props:
             html_prop_str = self.props_to_html()
             return f"<{self.tag}{html_prop_str}>{self.value}</{self.tag}>"
-        
+
         else:
             return f"<{self.tag}>{self.value}</{self.tag}>"
-        
+
 
 class ParentNode(HTMLNode):
     def __init__(self, tag, children, value=None, props=None):
         if not children:
             raise ValueError("Objects of class ParentNode must have children.")
-        
+
         if value:
             raise ValueError("Objects of class ParentNode cannot take a 'value' argument.")
-        
+
         if not tag:
             raise ValueError("Objects of class ParentNode must have a tag.")
 
@@ -67,32 +67,10 @@ class ParentNode(HTMLNode):
         child_obj_string = ""
         for obj in self.children:
             child_obj_string += obj.to_html()
-        
+
         if self.props:
             html_prop_str = self.props_to_html()
             return f"<{self.tag}{html_prop_str}>{child_obj_string}</{self.tag}>"
-        
+
         else:
             return f"<{self.tag}>{child_obj_string}</{self.tag}>"
-        
-
-node = ParentNode(
-    "p",
-    [
-        LeafNode(tag="b", value="Bold text"),
-        LeafNode(tag=None, value="Normal text"),
-        LeafNode(tag="i", value="italic text"),
-        LeafNode(tag=None, value="Normal text"),
-    ],
-)
-
-node2 = ParentNode(
-    tag="p",
-    children=[
-        LeafNode(tag=None, value="Start"),
-        node,
-        LeafNode(tag=None, value="End Wrapper Text")
-    ]
-)
-
-print(node2)
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode, LeafNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -29,6 +29,51 @@ class TestLeafNode(unittest.TestCase):
         props_to_html = leaf.props_to_html()
         self.assertIn(leaf.tag, test_to_html)
         self.assertIn(props_to_html, test_to_html)
+
+class TestParentNode(unittest.TestCase):
+    def test_children_required(self):
+        self.assertRaises(ValueError, ParentNode, tag="p", children=[])
+        self.assertRaises(ValueError, ParentNode, tag="p", children=None)
+
+    def test_no_value(self):
+        self.assertRaises(ValueError, ParentNode, value="test", tag="p", children=["child1"])
+
+    def test_tag_required(self):
+        self.assertRaises(ValueError, ParentNode, tag="", children=["child1"])
+        self.assertRaises(ValueError, ParentNode, tag=None, children=["child1"])
+
+    def test_to_html(self):
+        node = ParentNode(
+            "p",
+            [
+                LeafNode(tag="b", value="Bold text"),
+                LeafNode(tag=None, value="Normal text"),
+                LeafNode(tag="i", value="italic text"),
+                LeafNode(tag=None, value="Normal text"),
+            ],
+        )
+        to_html_output = node.to_html()
+        self.assertIsInstance(to_html_output, str)
+
+    def test_nested_ParentNode(self):
+
+        node2 = ParentNode(
+            tag="p",
+            children=[
+                LeafNode(tag="b", value="Bold text"),
+                LeafNode(tag=None, value="Normal text"),
+                LeafNode(tag="i", value="italic text"),
+                LeafNode(tag=None, value="Normal text"),
+            ],
+        )
+
+        node1 = ParentNode(tag="body", children=[node2])
+
+        full_html_str = node1.to_html()
+
+        self.assertIsInstance(full_html_str, str)
+        self.assertIn(node1.tag, full_html_str)
+        self.assertIn(node2.tag, full_html_str)
 
 if __name__ == "__main__":
     unittest.main()
