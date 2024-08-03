3fa45fc06ecc0b13edc05cdb701a58d4408a12b6
ajswetz <ajswetz@icloud.com>
2024-08-03 16:23:36 -0400

    update block_to_block_type.py

--- src/block_to_block_type.py
+++ src/block_to_block_type.py
@@ -29,7 +29,9 @@ def block_to_block_type(markdown_block):
     #UNORDERED LIST - Every line must start with a * or - character, followed by a space.
     is_unordered_list = True
     for line in md_block_lines:
-        if (line[0:2] != "* ") or (line[0:2] != "- "):
+        if (line[0:2] == "* ") or (line[0:2] == "- "):
+            is_unordered_list = True
+        else:
             is_unordered_list = False
             break
     if is_unordered_list == True:
@@ -41,9 +43,10 @@ def block_to_block_type(markdown_block):
     #               The number must start at 1 and increment by 1 for each line.
     is_ordered_list = True
     for i in range(len(md_block_lines)):
-        line_num = 1
         line = md_block_lines[i]
-        if line[0:3] != f"{line_num}. ":
+        line_num = i + 1
+        ol_syntax = f"{line_num}. "
+        if line[0:3] != ol_syntax:
             is_ordered_list = False
             break
     if is_ordered_list == True:
@@ -55,35 +58,35 @@ def block_to_block_type(markdown_block):
     return block_type
 
 
-test_heading = "# This is my heading"
-block_type = block_to_block_type(test_heading)
-print(f"heading block type: {block_type}")
+# test_heading = "# This is my heading"
+# block_type = block_to_block_type(test_heading)
+# print(f"heading block type: {block_type}")
 
-##########
+# ##########
 
-test_code = '''```
-my_text = "Hello World"
-print(my_text)
-```'''
-block_type = block_to_block_type(test_code)
-print(f"code block type: {block_type}")
+# test_code = '''```
+# my_text = "Hello World"
+# print(my_text)
+# ```'''
+# block_type = block_to_block_type(test_code)
+# print(f"code block type: {block_type}")
 
-##########
+# ##########
 
-test_quote = '''>It was the best of times
->It was the worst of times
->And I'm tired
->So I'm going to bed'''
+# test_quote = '''>It was the best of times
+# >It was the worst of times
+# >And I'm tired
+# >So I'm going to bed'''
 
-block_type = block_to_block_type(test_quote)
-print(f"quote block type: {block_type}")
+# block_type = block_to_block_type(test_quote)
+# print(f"quote block type: {block_type}")
 
 ##########
 
 test_unordered_list = '''* Item One
 - Item Two
 * Item Three
-- Item Four'''
+* Item Four'''
 
 block_type = block_to_block_type(test_unordered_list)
 print(f"unordered list block type: {block_type}")
@@ -95,14 +98,14 @@ test_ordered_list = '''1. Item Num 1
 3. Item Num 3
 4. Item Num 4'''
 
-block_type = block_to_block_type(test_unordered_list)
+block_type = block_to_block_type(test_ordered_list)
 print(f"ordered list block type: {block_type}")
 
-##########
+# ##########
 
-test_paragraph = '''One plain paragraph.
-Nothing Special.
-No unique block type.'''
+# test_paragraph = '''One plain paragraph.
+# Nothing Special.
+# No unique block type.'''
 
-block_type = block_to_block_type(test_paragraph)
-print(f"paragraph block type: {block_type}")
+# block_type = block_to_block_type(test_paragraph)
+# print(f"paragraph block type: {block_type}")
