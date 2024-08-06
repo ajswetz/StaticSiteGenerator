4888f774c8012a4af8242afe942771f053dad3fe
ajswetz <ajswetz@icloud.com>
2024-08-05 22:52:33 -0400

    update main.py to use generate_pages_recursive.py

--- src/main.py
+++ src/main.py
@@ -1,12 +1,13 @@
 from copy_static_to_public_recursively import *
 from generate_page import *
+from generate_pages_recursive import *
 
 def main():
 
     # Delete public dir, recreate public dir, and fill with copy of content in 'static'
     copy_static_to_public()
 
-    # Generate a page from content/index.md using template.html and write it to public/index.html.
-    generate_page(from_path="content/index.md", template_path="template.html", dest_path="public/index.html")
+    # Generate HTML pages recursively from "content", saved to "public"
+    generate_pages_recursive(dir_path_content="content", template_path="template.html", dest_dir_path="public")
 
 main()
